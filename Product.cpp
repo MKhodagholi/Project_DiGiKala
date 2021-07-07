f4f7ea8cdd75fcd78dfdb03fcb6e89e29354065f
Mohammad <MKhodaagholi@gmail.com>
2021-07-07 15:01:25 +0430

    [implemention function of class "Product"]

--- Product.cpp
+++ Product.cpp
@@ -1,1 +1,79 @@
 #include "Product.h"
+#include <iostream>
+
+Product::Product(std::string name, int price, int count, std::string description, int score, vector<std::string> comment)
+    : name(name), price(price), count(count), description(description), score(score), comment(comment) {}
+
+void Product::set_name(std::string name) {
+    this->name = name;
+}
+
+std::string Product::get_name() {
+    return name;
+}
+
+void Product::set_price(int price) {
+    this->price = price;
+}
+
+int Product::get_price() {
+    return price;
+}
+
+void Product::set_count(int count) {
+    this->count = count;
+}
+
+int Product::get_count() {
+    return count;
+}
+
+void Product::set_description(std::string description) {
+    this->description = description;
+}
+
+std::string Product::get_description() {
+    return description;
+}
+
+void Product::set_score(int score) {
+    this->score = score;
+}
+
+int Product::get_score() {
+    return score;
+}
+
+void Product::add_comment(std::string comment) {
+    this->comment.push_back(comment);
+}
+
+int Product::count_comments() {
+    return comment.size();
+}
+
+void Product::show_comments() {
+    for (int i = 0; i < comment.size(); i++)
+        std::cout << (i + 1) << ") " << comment[i] << std::endl;
+}
+
+std::string Product::get_comment(int num) {
+    if (num > -1 && num < comment.size())
+        return comment[comment.size()];
+    else if (abs(num) <= comment.size() && num < 0)
+        return comment[num + comment.size()];
+    else
+        throw OutOfRange;
+}
+
+std::ostream& Product::operator << (std::ostream &print, const Product &obj) {
+    print << "-------------------------------------------------------";
+    print << "| Name: " << name << " " << "Price: " << price << std::endl;
+    print << "| Count: " << count << " " << "Description: " << description << std::endl;
+    print << "| Score: " << score << std::endl;
+    print << "| Comments: " << std::endl;
+    for (int i = 0; i < comment.size(); i++)
+        std::print << "| \t" << (i + 1) << ") " << comment[i] << std::endl;
+
+    return print;
+}
--- Product.h
+++ Product.h
@@ -2,6 +2,7 @@
 #define PROJECT_DIGIKALA_PRODUCT_H
 #include <string>
 #include <vector>
+
 class Product {
 private:
     std::string name;
@@ -11,17 +12,23 @@ private:
     int score;
     std::vector<std::string> comment;
 public:
-    Product(std::string, int, int , std::string, int, vector<std::string>);
-    void set_name();
+    Product(std::string, int, int, std::string, int, vector<std::string>);
+    void set_name(std::string);
     std::string get_name();
-    void set_price();
+    void set_price(int);
     int get_price();
-    void set_count();
+    void set_count(int);
     int get_count();
-    void set_description();
-    std::string get_description();
-    void set_score();
+    void set_description(std::string);
+    std::string get_description(std::string);
+    void set_score(int);
     int get_score();
+    void add_comment(std::string);
+    int count_comments();
+    void show_comments();
+    std::string get_comment(int);
+    class OutOfRange();
+    std::ostream& operator << (std::ostream&, const Product&);
 };
 
 
