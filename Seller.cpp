12872445dcdf00d314f5b5c470cc634c614ae7f5
Mohammad <MKhodaagholi@gmail.com>
2021-07-08 04:26:39 +0430

    [[Add getter and setter], [operator"<<"], [add reader writer file] and [change "**products" to "vector product"] ]

--- Seller.cpp
+++ Seller.cpp
@@ -1,36 +1,78 @@
 #include "Seller.h"
+#include <fstream>
 
+Seller::Seller(std::string username, std::string password, std::string name, long wallet, vector<Product> products, long phoneNumber)
+        : username(username), password(password), name(name), wallet(wallet), phoneNumber(phoneNumber), count_typeProducts(products.size()), products(products) {}
 
-Seller::Seller(std::string username, std::string password, std::string name, long wallet, std::string **products, long phoneNumber, int count_products)
-        : username(username), password(password), name(name), wallet(wallet), phoneNumber(phoneNumber), count_products(count_products)
-{
-        products = new std::string*[count_products]();
-        for (int i = 0; i < count_products; i++) {
-            products[i] = new std::string();
-        }
-        set_products(products);
+void Seller::set_products(std::vector<Product> products) {
+    this->products = products;
 }
 
-void Seller::set_products(std::string **products) {
-    for (int i = 0; i < count_products; i++) {
-        *this->products[i] = *products[i];
-    }
+Product Seller::get_product(int num) {
+    if (num > -1 && num < count_typeProducts)
+        return products[num];
+    else if (num < 0 && abs(num) <= count_typeProducts)
+        return products[num + count_typeProducts];
+    else
+        throw products.OutOfRange;
 }
 
-Seller::Seller(const Seller &obj) : username(obj.username), password(obj.password), name(obj.name), wallet(obj.wallet), phoneNumber(obj.phoneNumber), count_products(obj.count_products)  {
-    products = new std::string*[count_products]();
-    for (int i = 0; i < count_products; i++) {
-        products[i] = new std::string();
-    }
-    set_products(obj.products);
+void Seller::add_product(Product product) {
+    products.push_back(product);
 }
 
-Seller::~Seller() {
-    for (int i = 0; i < count_products; i++) {
-        delete products[i];
-    }
-    delete [] products;
+void Seller::add_product(Product product) {
+    products.push_back(product);
 }
+
+void Seller::set_username(std::string username) {
+    this->username = username;
+}
+
+std::string Seller::get_username() {
+    return username;
+}
+
+void Seller::set_password(std::string password) {
+    this->password = password;
+}
+
+std::string Seller::get_password() {
+    return password;
+}
+
+void Seller::set_name(std::string name) {
+    this->name = name;
+}
+
+std::string Seller::get_name() {
+    return name;
+}
+
+void Seller::set_count_typeProducts(int count) {
+    count_typeProducts = count;
+}
+
+int Seller::get_count_typeProducts() {
+    return count_typeProducts;
+}
+
+void Seller::set_wallet(long wallet) {
+    this->wallet = wallet;
+}
+
+long Seller::get_wallet() {
+    return wallet;
+}
+
+void Seller::set_phoneNumber(long phoneNumber) {
+   this->phoneNumber = phoneNumber;
+}
+
+long Seller::get_phone_number() {
+    return phoneNumber;
+}
+
 
 bool Seller::registrationProducts(bool flag) {
     if (flag)
@@ -49,3 +91,30 @@ bool Seller::confirmationReturnProduct(bool flag) {
         return true;
     return false;
 }
+
+
+
+std::ostream& Seller::operator << (std::ostream &print, const Seller &obj) {
+    print << "Username: " << username << " Password: " << password << std::endl;
+    print << "Name: " << name << " Count Type-Products: " << count_typeProducts << std::endl;
+    print << "Phone number: " << phoneNumber;
+    print << products;
+
+    return print;
+}
+
+void Seller::write_file() {
+    std::ofstream writer("Seller.txt", std::ios::app);
+    writer << *this;
+    writer.close();
+}
+
+void Seller::read_file() {
+    std::ifstream reader("Seller.txt");
+    std::string lineString;
+    while (getline(reader, lineString))
+        std::cout << lineString;
+
+    reader.close();
+}
+
--- Seller.h
+++ Seller.h
@@ -1,26 +1,43 @@
 #ifndef PROJECT_DIGIKALA_SELLER_H
 #define PROJECT_DIGIKALA_SELLER_H
+#include <iostream>
 #include <string>
+#include <vector>
+#include "Product.h"
 
 class Seller {
 private:
     std::string username;
     std::string password;
     std::string name;
-    int count_products;
+    int count_typeProducts;
     long wallet;
-    std::string **products;
+    std::vector<Product> products;
     long phoneNumber;
 public:
-    Seller(std::string, std::string, std::string, long, std::string**, long, int);
-    Seller(const Seller &);
-    ~Seller();
-    void set_products(std::string**);
+    Seller(std::string, std::string, std::string, long, std::vector<Product>, long);
+    void set_products(std::vector<Product>);
+    Product get_product(int);
+    void add_product(Product);
+    void set_username(std::string);
+    std::string get_username();
+    void set_password(std::string);
+    std::string get_password();
+    void set_name(std::string);
+    std::string get_name();
+    void set_count_typeProducts(int);
+    int get_count_typeProducts();
+    void set_wallet(long);
+    long get_wallet();
+    void set_phoneNumber(long);
+    long get_phone_number();
     bool registrationProducts(bool);
     bool registreationCodeDiscuss(bool);
     bool confirmationReturnProduct(bool);
     void showSaleStatistics();
-
+    std::ostream& operator << (std::ostream&, const Seller&);
+    void write_file();
+    void read_file();
 };
 
 
