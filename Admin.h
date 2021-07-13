ffafa62fcb7f11f6c047b6b2fb39ebe1f054a3bb
SepehrNorouzi7 <snorouzi07@gmail.com>
2021-07-13 12:09:37 +0430

    add members of Admin class and library

---
+++ Admin.h
@@ -0,0 +1,16 @@
+#ifndef UNTITLED1_ADMIN_H
+#define UNTITLED1_ADMIN_H
+#include "Product.h"
+#include <sstream>
+using namespace std;
+
+class Admin {
+    string username;
+    string password;
+    long phoneNumber;
+    vector <Product> productOfAdmin;
+
+};
+
+
+#endif
